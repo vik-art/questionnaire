19d69405038b04a3087a93965c3b215839ebc5c4
Viktoriia <krjuchenkova@gmail.com>
2022-04-19 23:00:01 +0300

    dynamic components added

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -10,12 +10,9 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 export class FormComponent implements OnInit {
 
   form!: FormGroup;
+  versions: Array<string> = [];
 
-  technologies = [
-    { name: "Angular" },
-    { name: "React" },
-    { name: "Vue" }
-  ]
+  technologies = ["Angular", "React", "Vue"];
 
   constructor() { }
 
@@ -28,13 +25,26 @@ export class FormComponent implements OnInit {
       firstName: new FormControl("", [Validators.required]),
       lastName: new FormControl("", [Validators.required]),
       dateOfBirth: new FormControl("", [Validators.required]),
-      framework: new FormControl("", [Validators.required])
+      technology: new FormGroup({
+        framework: new FormControl("", Validators.required),
+        frameworkVersion: new FormControl("", Validators.required)
+      })
     })
     this.form.valueChanges.subscribe(() => {})
   }
 
   submit() {
     console.log(this.form.value)
+  }
+
+  setVersion() {
+    const frameworkVersion: {[key: string]: Array<string>} = {
+      'Angular': ['1.1.1', '1.2.1', '1.3.3'],
+      'React': ['2.1.2', '3.2.4', '4.3.1'],
+      'Vue': ['3.3.1', '5.2.1', '5.1.3']
+    }
+    const framework = this.form.get("technology")?.get("framework")?.value;
+    this.versions = frameworkVersion[framework];
   }
 
 }
